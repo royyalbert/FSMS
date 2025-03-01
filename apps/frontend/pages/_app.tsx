755992264cfc54c19da8d28d586c9b332d325591
royyalbert <ydn0499@gmail.com>
2025-03-01 15:20:52 +0200

    Initial commit - FSMS setup with Nx workspace

--- apps/frontend/pages/_app.tsx
+++ apps/frontend/pages/_app.tsx
@@ -10,7 +10,7 @@ function CustomApp({ Component, pageProps }: AppProps) {
       </Head>
       <main className="app">
         <Component {...pageProps} />
-      </main>
+      </main> 
     </>
   );
 }
